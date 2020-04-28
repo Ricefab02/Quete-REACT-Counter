aed5bd13c8f0b9e1c7346f613ddd771d5b91bf01
Fabrice ROGER <57527034+Ricefab02@users.noreply.github.com>
2020-04-28 10:46:17 +0200

    refactor(counter): "input with Number"

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 
 function Counter() {
-  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
-  // Déclare une fonction setCount qui permet de modifier la valeur de count
-  const [count, setCount] = useState(0);
-
+    const [count, setCount] = useState () 
   return (
-      
+         
     <div>
-      <>
-      counter = 0  
-      </>
-      <p>Le compteur est  à : {count} </p>
-      <button onClick={() => setCount(count + 1)}>
+        
+      <href >Entré une valeur :<input type="number" onChange={(event) => setCount(Number(event.target.value))}/></href>
+      
+      <p>Le compteur est  à : { count } </p>  
+      
+      <button onClick={() => setCount(count + 1) } >
         +1
       </button>
+      
       <button onClick={() => setCount(count - 1)}>
         -1
       </button>
+
+     
     </div>
   );
+
 }
 
 export default Counter;
